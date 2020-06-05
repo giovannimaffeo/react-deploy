b9b0740c4908bd4092131055fc4d5f4e123ea731
giovannimaffeo <giovanni.maffeo@gmail.com>
2020-06-05 19:05:46 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My react app</h1>
+      <h2>Author: Giovanni Maffeo</h2>
+    </div>
+  );
+}
+
+export default App;
