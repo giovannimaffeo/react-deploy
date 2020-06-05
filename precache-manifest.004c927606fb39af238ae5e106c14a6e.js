071898cdf5e7f92f2052dc6f24947cdc4c5cdc8e
giovannimaffeo <giovanni.maffeo@gmail.com>
2020-06-05 19:06:16 -0300

    Updates

---
+++ precache-manifest.004c927606fb39af238ae5e106c14a6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09600fb2ba4140b4e12aaecdfdab93a",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "5258081ab1f47ce13db6",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f15655fe87c942962f3c",
+    "url": "/react-deploy/static/js/2.027aa23c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.027aa23c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5258081ab1f47ce13db6",
+    "url": "/react-deploy/static/js/main.3e4bde66.chunk.js"
+  },
+  {
+    "revision": "9f4bfc363d6e5b7f3b4e",
+    "url": "/react-deploy/static/js/runtime-main.72e245fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
